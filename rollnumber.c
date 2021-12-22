04f87f29070e02df68eaadd675eab622c60ad5e9
Rohan Bhardwaj <94526837+rohanbhardwaj205@users.noreply.github.com>
2021-12-23 03:25:44 +0530

    Update rollnumber.c

--- rollnumber.c
+++ rollnumber.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
+int main()
 {
-    int a,i, b[100];
+    int a,i;
     printf("Enter the number of roll numbers you want to enter.\n");
     scanf("%d",&a);
     printf("Enter the value: ");
-    //int b[a]; // will create partition in form of b1, b2, b3, b4....
+    int b[100];
     for (i=1; i<=a; i++)
     {
-        scanf("%d",&b[a]);
+        scanf("%d",&b[i]);
         if (i<a)
         {
             printf("\n Enter next value: \n"); // will might get printed even when next value is not to be printed.
@@ -19,21 +19,35 @@ void main()
     {
         if (i==1)
         {
-            printf("%dst Entered roll number is: %d \n",i, b[a]);
+            printf("%dst Entered roll number is: %d \n",i, b[i]);
         }
           if (i==2)
         {
-            printf("%dnd Entered roll number is: %d \n",i, b[a]);
+            printf("%dnd Entered roll number is: %d \n",i, b[i]);
         }
           if (i==3)
         {
-            printf("%drd Entered roll number is: %d \n",i, b[a]);
+            printf("%drd Entered roll number is: %d \n",i, b[i]);
         }
           if (i>3)
         {
-            printf("%dth Entered roll number is: %d \n",i, b[a]);
+            if (i%10==1 && i!=11)
+            {
+                printf("%dst Entered roll number is: %d \n",i, b[i]);
+            }
+            if (i%10==2 && i!=12)
+            {
+                printf("%dnd Entered roll number is: %d \n",i, b[i]);
+            }
+            if (i%10==3 && i!=13)
+            {
+                printf("%drd Entered roll number is: %d \n",i, b[i]);
+            }
+            else
+            {
+                printf("%dth Entered roll number is: %d \n",i, b[i]);
+            }
         }
     }
-    //return 0;
-  getch();
+    return 0;
 }
